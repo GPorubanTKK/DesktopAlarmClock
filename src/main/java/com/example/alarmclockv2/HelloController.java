3177416efbc4d41261d08e5a4ece778b6bb74ecf
RandomLonelyDev <gedeon@porubannursery.com>
2022-09-20 09:13:50 -0400

    everything works as intended.  default file path must be updated for release

--- src/main/java/com/example/alarmclockv2/HelloController.java
+++ src/main/java/com/example/alarmclockv2/HelloController.java
@@ -1,14 +1,53 @@
 package com.example.alarmclockv2;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+import java.io.File;
 
 public class HelloController {
+    int hrs = 0;
+    int minutes = 0;
+    File file = new File("/C:/Users/Gedeon/Desktop/Locke's Files/AlarmClock v2/target/classes/com/example/alarmclockv2/default_alarm.wav");
     @FXML
-    private Label welcomeText;
-
+    public Label hours;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public Label mins;
+    @FXML
+    public TextField hrfield;
+    @FXML
+    public TextField minfield;
+    @FXML
+    public Text alarmstatus;
+    @FXML
+    protected void onSetClicked(){
+        try {
+            hrs = Integer.parseInt(hrfield.getText());
+            minutes = Integer.parseInt(minfield.getText());
+            HelloApplication.setAlarm(hrs,minutes,file);
+            String formattedminutes = minutes < 10?String.format("0%d",minutes):String.format("%d",minutes);
+            alarmstatus.setText(String.format("An alarm is set for %d:%s.",hrs,formattedminutes));
+        } catch(NumberFormatException e){
+            hrs = 0;
+            minutes = 0;
+            HelloApplication.setAlarm(hrs,minutes,file);
+            alarmstatus.setText("An alarm is set for midnight.");
+        }
+    }
+    @FXML
+    protected void onKillClicked(){
+        try {
+            HelloApplication.killAlarm();
+            alarmstatus.setText("You can sleep in =D");
+            System.out.print("\nAlarm removed successfully!\n");
+        } catch (NullPointerException e){
+            alarmstatus.setText("You can sleep in =D");
+        }
+    }
+    @FXML
+    protected void onSelectClicked(){
+        file = HelloApplication.filechooser();
+        System.out.format("\nthe audio file is %s\n",file.toString());
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,14 +2,12 @@ module com.example.alarmclockv2 {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
-
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.desktop;
 
     opens com.example.alarmclockv2 to javafx.fxml;
     exports com.example.alarmclockv2;
